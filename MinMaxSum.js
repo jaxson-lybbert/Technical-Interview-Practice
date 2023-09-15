2c8c57492d211e42463863ceb748dd055f94b836
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-09-15 16:02:30 -0600

    Wrote another solution using the .reduce() method

--- MinMaxSum.js
+++ MinMaxSum.js
@@ -1,3 +1,62 @@
+// "use strict";
+
+// process.stdin.resume();
+// process.stdin.setEncoding("utf-8");
+
+// let inputString = "";
+// let currentLine = 0;
+
+// process.stdin.on("data", function (inputStdin) {
+//   inputString += inputStdin;
+// });
+
+// process.stdin.on("end", function () {
+//   inputString = inputString.split("\n");
+
+//   main();
+// });
+
+// function readLine() {
+//   return inputString[currentLine++];
+// }
+
+// /*
+//   Complete the 'miniMaxSum' function below.
+
+//  The function accepts INTEGER_ARRAY arr as parameter.
+//  */
+// let arr = [1, 2, 3, 4, 5];
+
+// function miniMaxSum(arr) {
+//   let maxSum = 0;
+//   let minSum = 0;
+
+//   arr = arr.sort((a, b) => {
+//     return a - b;
+//   });
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i != arr.length - 1) {
+//       minSum += arr[i];
+//     }
+
+//     if (i != 0) {
+//       maxSum += arr[i];
+//     }
+//   }
+
+//   console.log(minSum + " " + maxSum);
+// }
+
+// function main() {
+//   const arr = readLine()
+//     .replace(/\s+$/g, "")
+//     .split(" ")
+//     .map((arrTemp) => parseInt(arrTemp, 10));
+
+//   miniMaxSum(arr);
+// }
+
 "use strict";
 
 process.stdin.resume();
@@ -28,24 +87,13 @@ function readLine() {
 let arr = [1, 2, 3, 4, 5];
 
 function miniMaxSum(arr) {
-  let maxSum = 0;
-  let minSum = 0;
-
   arr = arr.sort((a, b) => {
     return a - b;
   });
 
-  for (let i = 0; i < arr.length; i++) {
-    if (i != arr.length - 1) {
-      minSum += arr[i];
-    }
+  let arrSum = arr.reduce((a, b) => a + b);
 
-    if (i != 0) {
-      maxSum += arr[i];
-    }
-  }
-
-  console.log(minSum + " " + maxSum);
+  console.log(arrSum - arr[arr.length - 1] + " " + (arrSum - arr[0]));
 }
 
 function main() {
