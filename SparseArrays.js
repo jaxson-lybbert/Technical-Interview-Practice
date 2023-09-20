dc98f31fec703aaed55015739002d28617d2996f
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-09-20 13:35:28 -0600

    first attempt

--- SparseArrays.js
+++ SparseArrays.js
@@ -32,7 +32,19 @@ function readLine() {
  */
 
 function matchingStrings(strings, queries) {
-  // Write your code here
+  let numOfQueries = [];
+  let queryMatch = 0;
+
+  for (let i = 0; i < strings.length; i++) {
+    for (let j = 0; j < queries.length; j++) {
+      if (strings[i] === queries[j]) {
+        queryMatch += 1;
+      }
+    }
+    numOfQueries.push(queryMatch);
+  }
+
+  return numOfQueries;
 }
 
 function main() {
