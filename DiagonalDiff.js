fa5a2615576ce364e3258be556c50155566259f5
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-10-20 23:45:54 -0600

    Forgot to add values

--- DiagonalDiff.js
+++ DiagonalDiff.js
@@ -34,8 +34,8 @@ function diagonalDifference(arr) {
   let rightToLeft = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    leftToRight = arr[i][i];
-    rightToLeft = arr[i][arr.length - i - 1];
+    leftToRight += arr[i][i];
+    rightToLeft += arr[i][arr.length - i - 1];
   }
 
   return Math.abs(leftToRight - rightToLeft);
